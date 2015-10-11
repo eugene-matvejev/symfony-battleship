205b90124fbd784b06a0583a01a1ce8159414ad8
Eugene Matvejev <eugene.matvejevs@babylonhealth.com>
2015-10-11 15:13:12 +0100

    init commit

---
+++ src/AppBundle/Library/Traits/Nameable.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Library\Traits;
+
+trait Identifiable {
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
