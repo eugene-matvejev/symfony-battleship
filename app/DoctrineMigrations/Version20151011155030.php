a03e2f7b4eb27e337a30c0794e97fdc218103f91
Eugene Matvejev <eugene.matvejevs@babylonhealth.com>
2015-10-11 15:51:24 +0100

    typo

---
+++ app/DoctrineMigrations/Version20151011155030.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151011155030 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cellState CHANGE amount name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE players CHANGE amount name VARCHAR(255) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cellState CHANGE name amount VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE players CHANGE name amount VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
