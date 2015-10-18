a8bc69b3516de40e74349758a1562720df0c9762
Eugene Matvejev <eugene.matvejev@gmail.com>
2015-10-18 21:16:55 +0100

    start polish.

--- src/AppBundle/Resources/public/js/_page.controller.js
+++ src/AppBundle/Resources/public/js/_page.controller.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
-    var pageMgr = new PageMgr();
-    $('.toggle-btn, .switch-btn').click(function(e) {
-        e.preventDefault();
-       pageMgr.toggleSidebar();
-    });
+    var pageMgr = (new PageMgr())
+                    .loadingMode(true)
+                    .toggleTitle(document.querySelector('li[data-action="game-new"]'));
+
+    $('.page-sidebar, .page-content')
+        .on('click', '.toggle-btn, .switch-btn', function(e) {
+            e.preventDefault();
+           pageMgr.toggleSidebar();
+        });
+
+    $('.page-sidebar')
+        .on('click', 'li[data-section], li[data-action]', function(e) {
+            e.preventDefault();
+            pageMgr.switchSection(this);
+        });
+
 });
 
--- src/AppBundle/Resources/public/js/prototypes/gui/page.mgr.protorype.js
+++ src/AppBundle/Resources/public/js/prototypes/gui/page.mgr.protorype.js
@@ -1,13 +1,58 @@
 function PageMgr() {
-    this.$sidebar = $('div.page-sidebar');
-    this.$content = $('div.page-content');
+    this.$docTitle  = $('head>title');
+    this.$container = $('div.container');
+    this.$loading   = this.$container.find('div.page-loading');
+    this.$sidebar   = this.$container.find('div.page-sidebar');
+    this.$content   = this.$container.find('div.page-content');
+    this.$pageTitle = this.$content.find('.page-section-title');
 }
 
 PageMgr.prototype = {
     toggleSidebar: function() {
-        this.$content.toggleClass(PageMgr.toggle);
-        this.$sidebar.toggleClass(PageMgr.toggle);
+        this.$content.toggleClass(PageMgr.classes.toggle);
+        this.$sidebar.toggleClass(PageMgr.classes.toggle);
+    },
+    switchSection: function(el) {
+        console.log(el);
+        this.toggleTitle(el);
+        switch(el.getAttribute(PageMgr.index.action)) {
+            case PageMgr.action.game._new:
+                break;
+            default:
+                switch(el.getAttribute(PageMgr.index.section)) {
+                    case PageMgr.section.game:
+                        //this.toggleTitle($(this));
+                        break;
+                    case PageMgr.section.stats:
+                        break;
+                }
+                break;
+        }
+    },
+    toggleTitle: function(el) {
+        var postfix = el.innerText,//.text(),
+            prefix  = this.$sidebar.find('.' + PageMgr.classes.title).text();
+        //var postfix = $el.text(),
+        //    prefix  = this.$sidebar.find('.' + PageMgr.classes.title).text();
+        this.$docTitle.text(prefix + ' :: ' + postfix);
+        this.$pageTitle.text(postfix);
+
+        return this;
+    },
+    loadingMode: function(enable) {
+        if(enable) {
+            this.$loading.removeClass(PageMgr.classes.hidden);
+            this.$container.addClass(PageMgr.classes.locked);
+        } else {
+            this.$loading.addClass(PageMgr.classes.hidden);
+            this.$container.removeClass(PageMgr.classes.locked);
+        }
+
+        return this;
     }
 };
 
-PageMgr.toggle = 'toggled';
+PageMgr.index   = { action: 'data-action', section: 'data-section' };
+PageMgr.classes = { toggle: 'toggled', title: 'page-header', hidden: 'hidden', locked: 'no-scroll-mode' };
+PageMgr.section = { game: 'game-area', stats: 'stats-area' };
+PageMgr.action  = { game: { _new: 'game-new' } };
