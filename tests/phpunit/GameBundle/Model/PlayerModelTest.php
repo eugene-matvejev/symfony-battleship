f52368d9d068462ae35a20f373197cafe169964a
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-02-19 18:45:43 +0000

    more tests

--- tests/phpunit/GameBundle/Model/PlayerModelTest.php
+++ tests/phpunit/GameBundle/Model/PlayerModelTest.php
@@ -1,9 +1,64 @@
 <?php
-//
-//namespace EM\Tests\PHPUnit\GameBundle\Model;
-//
-//use EM\Tests\PHPUnit\Environment\ExtendedTestCase;
-//
-//class PlayerModelTest extends ExtendedTestCase
-//{
-//}
+
+namespace EM\Tests\PHPUnit\GameBundle\Model;
+
+use EM\GameBundle\Model\PlayerModel;
+use EM\Tests\PHPUnit\Environment\ExtendedTestCase;
+
+/**
+ * @see PlayerModel
+ */
+class PlayerModelTest extends ExtendedTestCase
+{
+    /**
+     * @var PlayerModel
+     */
+    private $playerModel;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->playerModel = $this->getContainer()->get('battleship.game.services.player.model');
+    }
+
+    /**
+     * @see PlayerModel::TYPE_CPU
+     * @test
+     */
+    public function playerTypeCPU()
+    {
+        $this->assertNotEquals(PlayerModel::TYPE_HUMAN, PlayerModel::TYPE_CPU);
+        $this->assertContains(PlayerModel::TYPE_CPU, PlayerModel::TYPES_ALL);
+    }
+
+    /**
+     * @see PlayerModel::TYPE_HUMAN
+     * @test
+     */
+    public function playerTypeHuman()
+    {
+        $this->assertNotEquals(PlayerModel::TYPE_CPU, PlayerModel::TYPE_HUMAN);
+        $this->assertContains(PlayerModel::TYPE_HUMAN, PlayerModel::TYPES_ALL);
+    }
+
+    /**
+     * @see PlayerModel::ALL_TYPES
+     * @test
+     */
+    public function playerTypesAll()
+    {
+        $this->assertCount(2, PlayerModel::TYPES_ALL);
+    }
+
+    /**
+     * @see PlayerModel::getTypes
+     * @test
+     */
+    public function getTypes()
+    {
+        $this->assertCount(2, $this->playerModel->getTypes());
+        foreach ($this->playerModel->getTypes() as $playerType) {
+            $this->assertContains($playerType->getId(), PlayerModel::TYPES_ALL);
+        }
+    }
+}
