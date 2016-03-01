8578fa84013a0997640477f74f2dacbe8e363abb
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-03-01 23:05:52 +0000

    ECMA Script 6  Support

---
+++ src/GameBundle/Resources/public/ecma5/_statistics.controller.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var statisticsMgr = new Statistics();
+
+    $('.page-sidebar')
+        .on('click', 'li[data-section="stats-area"]', function(e) {
+            e.stopPropagation();
+
+            statisticsMgr.pageMgr.switchSection(this);
+            statisticsMgr.fetch(1);
+        });
+
+    $('div#stats-area')
+        .on('click', 'button[data-page]', function(e) {
+            e.stopPropagation();
+
+            var page = this.getAttribute('data-page');
+            statisticsMgr.fetch(page);
+        });
+});
