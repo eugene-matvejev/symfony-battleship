b534bafa266007e80a9314bf43dc9b40d6a8440f
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-03-21 23:39:40 +0000

    keep play with behat

---
+++ tests/behat/context/GameBundle/Controller/GameResultControllerContext.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EM\Tests\Behat\GameBundle\Controller;
+
+use EM\GameBundle\Controller\GameResultController;
+use EM\Tests\PHPUnit\Environment\ExtendedTestCase;
+
+/**
+ * @see GameResultController
+ */
+class GameResultControllerContext //extends ExtendedTestCase
+{
+}
