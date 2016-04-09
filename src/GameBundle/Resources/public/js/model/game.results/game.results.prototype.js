8b8b3e98b8bbc0a9ba5c5eb879337d4aa618ac3a
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-04-10 00:05:50 +0100

    simplify GameResults

--- src/GameBundle/Resources/public/js/model/game.results/game.results.prototype.js
+++ src/GameBundle/Resources/public/js/model/game.results/game.results.prototype.js
@@ -1,63 +1,68 @@
 'use strict';
 
-class GameResults {
+class GameResults extends APIRequestMgr {
     /**
      * @param {jQuery} $el
      */
     constructor($el) {
-        this.$html      = $el;
-        this.apiMgr     = new APIRequestMgr();
-        this.pageMgr    = new PageMgr();
-        this.pagination = new PaginationMgr(this.$html);
+        super();
+
+        this.pagination = new PaginationMgr();
+        this.route      = $el.attr('data-game-results-link');
+
+        this.$tableArea = $(this.constructor.resources.layout);
+        console.log(this.$tableArea);
+        $el.append(this.$tableArea, this.pagination.$html);
     }
 
     /**
      * @param {int|string} page
      */
     fetch(page) {
-        let self       = this,
-            requestUrl = this.$html.attr('data-game-results-link') + page,
-            onSuccess  = function (response) {
-                self.updateHTML(response);
+        let self      = this,
+            onSuccess = function (response) {
+                self.update(response);
             };
 
-        this.apiMgr.request('GET', requestUrl, undefined, onSuccess);
+        super.request('GET', this.route + page, undefined, onSuccess);
     }
 
     /**
      * @param {{meta: {currentPage: {int}, totalPages: {int}}, results: []}} response
      */
-    updateHTML(response) {
-        let html   = GameResults.resources.html,
-            $table = $(html.layout());
+    update(response) {
+        let html   = this.constructor.resources.html,
+            $table = $(html.table()),
+            $tBody = $table.find('tbody');
 
-        response.results.map(result => $table.append(html.row(result)));
+        response.results.forEach(result => $tBody.append(html.row(result)));
 
-        this.$html.html($table).append(PaginationMgr.resources.html.layout);
-        this.pagination.updateHTML(response.meta.currentPage, response.meta.totalPages);
+        this.$tableArea.html($table);
+        this.pagination.update(response.meta.currentPage, response.meta.totalPages);
     }
 }
 
 GameResults.resources = {};
 /** @enum {string} */
-GameResults.resources.text = {
-    id: 'id',
-    player: 'winner',
-    time: 'finished at'
+GameResults.resources.tableHeader = {
+    resultId: '#',
+    playerName: 'winner',
+    finishTime: 'finished at'
 };
-GameResults.resources.html = {
+GameResults.resources.layout = '<div class="results-area"></div>';
+GameResults.resources.html   = {
     /**
      * @returns {string}
      */
-    layout: function () {
-        let text = GameResults.resources.text;
+    table: function () {
+        let header = GameResults.resources.tableHeader;
 
-        return '\
+        return ' \
             <table class="table"> \
                 <tr> \
-                    <th>' + text.id + '</th> \
-                    <th>' + text.time + '</th> \
-                    <th>' + text.player + '</th> \
+                    <th>' + header.resultId + '</th> \
+                    <th>' + header.playerName + '</th> \
+                    <th>' + header.finishTime + '</th> \
                 </tr> \
             </table>';
     },
@@ -67,12 +72,10 @@ GameResults.resources.html = {
      * @returns {string}
      */
     row: function (obj) {
-        let date = new Date(obj.timestamp);
-
         return ' \
             <tr> \
                 <td>' + obj.id + '</td> \
-                <td>' + date.toLocaleString() + '</td> \
+                <td>' + (new Date(obj.timestamp)).toLocaleString() + '</td> \
                 <td>' + obj.player.name + '</td> \
             </tr>';
     }
