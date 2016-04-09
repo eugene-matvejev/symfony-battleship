dd312e0a48cff5ca9ad850f878b210d9119b0d3a
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-04-09 23:45:35 +0100

    simplify pagination

--- src/GameBundle/Resources/public/js/model/gui/pagination.mgr.prototype.js
+++ src/GameBundle/Resources/public/js/model/gui/pagination.mgr.prototype.js
@@ -1,77 +1,82 @@
 'use strict';
 
 class PaginationMgr {
-    /**
-     * @param {jQuery} $el
-     */
-    constructor($el) {
-        this.$area = $el;
+    constructor() {
+        this.previousPage = this.currentPage = this.totalPages = this.nextPage = 1;
+
+        this.$html = $(this.constructor.resources.layout);
+
+        this.$currentPage = this.$html.find('span#data-page-current');
+        this.$totalPages  = this.$html.find('span#data-page-total');
+
+        this.$nextPageBtn     = this.$html.find('button#next-page-btn');
+        this.$previousPageBtn = this.$html.find('button#previous-page-btn');
     }
 
     /**
-     * @param {int|string} currPage
-     * @param {int|string} totalPages
+     * @param {int} currPage
+     * @param {int} totalPages
      */
-    updateHTML(currPage, totalPages) {
-        let attr     = PaginationMgr.resources.config.attr,
-            nextPage = currPage + 1 > totalPages ? totalPages : currPage + 1,
-            prevPage = currPage - 1 <= 0 ? 1 : currPage - 1;
+    update(currPage, totalPages) {
+        this.setTotalPages(totalPages);
+        this.setCurrentPage(currPage);
 
-        this.$area.find('button[type="button"]').each(function () {
-            switch (this.id) {
-                case attr.id.curr:
-                    $(this).find('>span:first-child').html(currPage);
-                    $(this).find('>span:last-child').html(totalPages);
-                    this.setAttribute(attr.page, currPage);
-                    break;
-                case attr.id.prev:
-                    if (currPage == 1) {
-                        this.setAttribute('disabled', 'disabled');
-                    }
-                    this.setAttribute(attr.page, prevPage);
-                    break;
-                case attr.id.next:
-                    if (currPage === totalPages) {
-                        this.setAttribute('disabled', 'disabled');
-                    }
-                    this.setAttribute(attr.page, nextPage);
-                    break;
-            }
-        });
+        this.setPreviousPage(this.currentPage - 1);
+        this.setNextPage(this.currentPage + 1);
+    }
+
+    /**
+     * @param {int} page
+     */
+    setPreviousPage(page) {
+        this.previousPage = page;
+
+        this.$previousPageBtn.attr("data-page", this.previousPage)[0].disabled = this.previousPage < 1;
+    }
+
+    /**
+     * @param {int} page
+     */
+    setCurrentPage(page) {
+        this.currentPage = page;
+
+        this.$currentPage.text(this.currentPage);
+    }
+
+    /**
+     * @param {int} page
+     */
+    setTotalPages(page) {
+        this.totalPages = page;
+
+        this.$totalPages.text(this.totalPages);
+    }
+
+    /**
+     * @param {int} page
+     */
+    setNextPage(page) {
+        this.nextPage = page;
+
+        this.$nextPageBtn.attr("data-page", this.nextPage)[0].disabled = this.nextPage > this.totalPages;
     }
 }
 
-PaginationMgr.resources        = {};
-PaginationMgr.resources.config = {
-    attr: {
-        /** @enum {string} */
-        id: {
-            prev: 'prev',
-            curr: 'curr',
-            next: 'next',
-            total: 'total'
-        },
-        /** @type {string} */
-        page: 'data-page'
-    }
-};
-PaginationMgr.resources.html   = {
-    /**
-     * @returns {string}
-     */
+PaginationMgr.resources = {
+    /** @type {string} */
     layout: '\
         <div class="pagination-area"> \
             <div class="btn-group btn-group-xs" role="group" aria-label="statistics-pagination"> \
-                <button id="prev" type="button" class="btn btn-default"> \
-                    <span class="glyphicon glyphicon-chevron-left"></span> \
+                <button type="button" id="previous-page-btn" data-page="" class="btn btn-default"> \
+                    <span id="data-page-previous" class="glyphicon glyphicon-chevron-left"></span> \
                 </button> \
-                <button id="curr" type="button" class="btn btn-default" disabled="disabled"> \
-                    <span></span> \
+                <button type="button" class="btn btn-default" disabled="disabled"> \
+                    <span id="data-page-current"></span> \
                     <span> of </span> \
-                    <span></span> \
+                    <span id="data-page-total""></span> \
                 </button> \
-                <button id="next" type="button" class="btn btn-default"> \
-                    <span class="glyphicon glyphicon-chevron-right"></span> \
+                <button type="button" id="next-page-btn" data-page="" class="btn btn-default"> \
+                    <span id="data-page-next" class="glyphicon glyphicon-chevron-right"></span> \
                 </button> \
             </div> \
         </div>'
