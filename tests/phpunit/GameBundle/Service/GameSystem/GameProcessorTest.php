994aeacc5e0ab84f9cc65ee16d3393b145152205
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-04-10 19:07:29 +0100

    Revert "simplify game model"
    This reverts commit c2dabc3522527f8316ecbbd5116bedf70a990c89.

--- tests/phpunit/GameBundle/Service/GameSystem/GameProcessorTest.php
+++ tests/phpunit/GameBundle/Service/GameSystem/GameProcessorTest.php
@@ -136,6 +136,22 @@ class GameProcessorTest extends ExtendedTestSuite
      * @see     GameProcessor::processPlayerTurn
      * @test
      *
+     * @expectedException \EM\GameBundle\Exception\CellException
+     *
+     * @depends processGameTurnOnUnfinishedGame
+     */
+    public function processPlayerTurnThrowsCellException()
+    {
+        $battlefield = $this->getBattlefieldMock()
+            ->setPlayer($this->getPlayerMock('', $this->getPlayerTypeMock(PlayerModel::TYPE_CPU)));
+
+        $this->invokeProcessPlayerTurnMethod([$battlefield, 'A0']);
+    }
+
+    /**
+     * @see     GameProcessor::processPlayerTurn
+     * @test
+     *
      * @expectedException \EM\GameBundle\Exception\PlayerException
      *
      * @depends processGameTurnOnUnfinishedGame
