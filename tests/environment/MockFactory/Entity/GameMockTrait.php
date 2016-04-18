ee61b37f0b032f632b262f74e996e7a4f5d0ada0
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-04-18 19:17:43 +0100

    improve GameResult mock factory and GameResult tests

--- tests/environment/MockFactory/Entity/GameMockTrait.php
+++ tests/environment/MockFactory/Entity/GameMockTrait.php
@@ -11,12 +11,11 @@ trait GameMockTrait
 {
     use BattlefieldMockTrait;
 
-    protected function getGameMock(int $battlefields = 2) : Game
+    protected function getGameMock(int $players = 2, int $size = 7) : Game
     {
         $game = new Game();
-
-        for ($i = 0; $i < $battlefields; $i++) {
-            $game->addBattlefield($this->getBattlefieldMock());
+        for ($i = 0; $i < $players; $i++) {
+            $game->addBattlefield($this->getBattlefieldMock($size));
         }
 
         return $game;
