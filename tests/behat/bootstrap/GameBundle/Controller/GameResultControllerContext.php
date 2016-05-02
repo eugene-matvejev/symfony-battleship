e229dbd0dafd628aa967e344d714f0cea8f09c0e
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-05-02 15:39:28 +0100

    save progress

--- tests/behat/bootstrap/GameBundle/Controller/GameResultControllerContext.php
+++ tests/behat/bootstrap/GameBundle/Controller/GameResultControllerContext.php
@@ -2,7 +2,6 @@
 
 namespace EM\Tests\Behat\GameBundle\Controller;
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Testwork\Hook\Call as Behat;
@@ -12,17 +11,17 @@ use Symfony\Component\HttpFoundation\Request;
 class GameResultControllerContext extends AbstractContainerAwareContext implements Context, SnippetAcceptingContext
 {
     /**
-     * @Given I am requesting :route with :param and :value API endpoint
+     * @Given requesting :route with :paramKey and :paramValue API endpoint
      *
      * @param string $route
-     * @param string $param
-     * @param string $value
+     * @param string $paramKey
+     * @param string $paramValue
      */
-    public function iAmRequestingWithAndApiEndpoint(string $route, string $param, string $value)
+    public function requestingWithAndApiEndpoint(string $route, string $paramKey, string $paramValue)
     {
         $this->_client->request(
             Request::METHOD_GET,
-            static::$router->generate($route, [$param => $value]),
+            static::$router->generate($route, [$paramKey => $paramValue]),
             [],
             [],
             ['CONTENT_TYPE' => 'application/json', 'HTTP_accept' => 'application/json']
@@ -30,25 +29,25 @@ class GameResultControllerContext extends AbstractContainerAwareContext implemen
     }
 
     /**
-     * @Then I should get successful response
+     * @Then observe successful response
      */
-    public function iShouldGetSuccessfulResponse()
+    public function observeSuccessfulResponse()
     {
         $this->assertSuccessfulJSONResponse($this->_client->getResponse());
     }
 
     /**
-     * @Then there should be :arg1 results
+     * @Then there should be :page and have :expectedAmountOfResults results
+     *
+     * @param int $page
+     * @param int $expectedAmountOfResults
      */
-    public function thereShouldBeResults(string $page, string $results)
+    public function thereShouldBeAndHaveResults(int $page, int $expectedAmountOfResults)
     {
-        $parsedJson = json_decode($this->_client->getResponse()->getContent());
+        $response = json_decode($this->_client->getResponse()->getContent());
 
-        $parsedJson;
-        if('asd' != 1) {
-
-        }
-        $this->assertInstanceOf(\stdClass::class, $parsedJson->meta);
-        $this->assertEquals()
+        $this->assertInstanceOf(\stdClass::class, $response->meta);
+        $this->assertEquals($page, $response->meta->currentPage);
+        $this->assertCount($expectedAmountOfResults, $response->results);
     }
 }
--- tests/behat/features/api/api.game_results.feature
+++ tests/behat/features/api/api.game_results.feature
@@ -4,15 +4,12 @@ Feature: Game Results API
     Given setup context
 
   Scenario Outline: request API
-    Given I am requesting "<route>" with "<argument>" and "<value>" API endpoint
-    Then I should get successful response
-    And there should be <results> results
+    Given requesting "<route>" with "<arg>" and "<argValue>" API endpoint
+    Then observe successful response
+    And there should be "<argValue>" and have "<expectedAmountOfGameResults>" results
 
     Examples:
-      | route                            | argument | value | results |
-      | battleship.game.api.game.results | page     | 1     | 0       |
-      | battleship.game.api.game.results | page     | 2     | 0       |
-      | battleship.game.api.game.results | page     | 99    | 0       |
-
-#  Scenario: request API
-#    Given all ok
+      | route                            | arg  | argValue | expectedAmountOfGameResults |
+      | battleship.game.api.game.results | page | 1        | 0                           |
+      | battleship.game.api.game.results | page | 2        | 0                           |
+      | battleship.game.api.game.results | page | 99       | 0                           |
