5bd13cc353c706f6e98a677cd5e78061fad642e7
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-05-03 21:24:47 +0000

    put index on player name

---
+++ app/DoctrineMigrations/Version20160503154515.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160503154515 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE players CHANGE name name VARCHAR(100) NOT NULL');
+        $this->addSql('CREATE INDEX INDEX_PLAYER_NAME ON players (name)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX INDEX_PLAYER_NAME ON players');
+        $this->addSql('ALTER TABLE players CHANGE name name VARCHAR(200) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
