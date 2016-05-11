293e3680620f66857480cfb2f5566a0382441070
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-05-11 22:23:26 +0100

    PHPDoc

--- src/GameBundle/Controller/AbstractAPIController.php
+++ src/GameBundle/Controller/AbstractAPIController.php
@@ -10,6 +10,13 @@ use Symfony\Component\HttpFoundation\Response;
  */
 abstract class AbstractAPIController extends Controller
 {
+    /**
+     * @param mixed    $data
+     * @param int      $status
+     * @param string[] $headers
+     *
+     * @return Response
+     */
     protected function prepareSerializedResponse($data, int $status = Response::HTTP_OK, array $headers = []) : Response
     {
         $header = $this->get('request_stack')->getMasterRequest()->headers->get('accept');
