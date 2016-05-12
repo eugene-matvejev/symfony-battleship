05bd81ca07c5a4ffdb09194cf865990803fae1df
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-05-12 06:06:31 +0100

    save progress

--- src/GameBundle/Response/GameResultsResponse.php
+++ src/GameBundle/Response/GameResultsResponse.php
@@ -3,42 +3,49 @@
 namespace EM\GameBundle\Response;
 
 use EM\GameBundle\Entity\GameResult;
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * EM\GameBundle\Response\GameResultsResponse:
+ * accessor_order: custom
+ * custom_accessor_order: [meta, results]
+ *
+ * xml_root_name: statistics
+ *
+ * properties:
+ * meta:
+ * type: array<string, integer>
+ * inline: false
+ * xml_key_value_pairs: true
+ * results:
+ * type: array<EM\GameBundle\Entity\GameResult>
+ * xml_list:
+ * inline: false
+ * entry_name: result
+ */
 
 /**
  * @since 5.0
+ *
+ * @JMS\AccessorOrder("custom", custom={"results","meta"})
+ * @JMS\XmlRoot("statistics")
  */
 class GameResultsResponse
 {
     const META_INDEX_CURRENT_PAGE = 'currentPage';
-    const META_INDEX_TOTAL_PAGES  = 'totalPages';
+    const META_INDEX_TOTAL_PAGES = 'totalPages';
     /**
-     * @var GameResult[]
-     */
-    private $results = [];
-    /**
+     * @JMS\Type("array<string, integer>")
+     * @JMS\XmlKeyValuePairs()
+     *
      * @var int[]
      */
     private $meta = [];
-
     /**
-     * @return GameResult[]
-     */
-    public function getResults() : array
-    {
-        return $this->results;
-    }
-
-    /**
-     * @param GameResult[] $results
      *
-     * @return $this
+     * @var GameResult[]
      */
-    public function setResults(array $results) : self
-    {
-        $this->results = $results;
-
-        return $this;
-    }
+    private $results = [];
 
     /**
      * @return int[]
@@ -67,5 +74,25 @@ class GameResultsResponse
         $this->setMeta(self::META_INDEX_CURRENT_PAGE, $value);
 
         return $this;
+    }
+
+    /**
+     * @param GameResult[] $results
+     *
+     * @return GameResultsResponse
+     */
+    public function setResults(array $results) : self
+    {
+        $this->results = $results;
+
+        return $this;
+    }
+
+    /**
+     * @return GameResult[]
+     */
+    public function getResults() : array
+    {
+        return $this->results;
     }
 }
