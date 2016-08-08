4923a133375147a45a2c32a0b5973438c41ddc20
Eugene Matvejevs <eugene.matvejev@gmail.com>
2016-08-08 18:13:12 +0100

    auhotization prototype: optimze MySQL migration

--- app/DoctrineMigrations/Version20160724000001.php
+++ app/DoctrineMigrations/Version20160724000001.php
@@ -21,7 +21,7 @@ class Version20160724000001 extends AbstractMigration
         $this->addSql('CREATE TABLE player_sessions (id INT AUTO_INCREMENT NOT NULL, player INT NOT NULL, hash VARCHAR(40) NOT NULL, timestamp DATETIME NOT NULL, INDEX IDX_9CB7DCF398197A65 (player), INDEX INDEX_SESSION_HASH (hash), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE player_sessions ADD CONSTRAINT FK_9CB7DCF398197A65 FOREIGN KEY (player) REFERENCES players (id)');
         $this->addSql('DROP INDEX INDEX_PLAYER_NAME ON players');
-        $this->addSql('ALTER TABLE players ADD passwordHash VARCHAR(40) NOT NULL, CHANGE name email VARCHAR(25) NOT NULL');
+        $this->addSql('ALTER TABLE players ADD passwordHash VARCHAR(40) NOT NULL, CHANGE name email VARCHAR(255) NOT NULL');
         $this->addSql('CREATE INDEX INDEX_PLAYER_EMAIL ON players (email)');
         $this->addSql('CREATE INDEX INDEX_PLAYER_EMAIL_AND_PASSWORD ON players (email, passwordHash)');
     }
