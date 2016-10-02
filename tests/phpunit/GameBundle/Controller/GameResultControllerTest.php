45c266f52245bc717938de362618fedc08b2e9bb
Eugene Matvejev <eugene.matvejev@gmail.com>
2016-10-02 23:42:26 +0100

    Update GameResultControllerTest.php

--- tests/phpunit/GameBundle/Controller/GameResultControllerTest.php
+++ tests/phpunit/GameBundle/Controller/GameResultControllerTest.php
@@ -35,7 +35,6 @@ class GameResultControllerTest extends AbstractControllerTestCase
     public function orderedByDateAction($pageId, int $expectedResponseCode)
     {
         $client = static::$client;
-        $pageId = 1;
         $client->request(
             Request::METHOD_GET,
             "/api/game-results/page/{$pageId}",
