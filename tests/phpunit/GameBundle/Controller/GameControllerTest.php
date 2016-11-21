b3e3e534d3a97f925cf21e7c53b626d659454393
Eugene Matvejevs <eugene.matvejev@gmail.com>
2016-11-21 04:24:43 +0000

    fix build

--- tests/phpunit/GameBundle/Controller/GameControllerTest.php
+++ tests/phpunit/GameBundle/Controller/GameControllerTest.php
@@ -47,7 +47,7 @@ class GameControllerTest extends AbstractControllerTestCase
             [],
             [],
             ['CONTENT_TYPE' => 'application/json', 'HTTP_accept' => 'application/json'],
-            static::getSharedFixtureContent('game-initiation-requests/valid/2-players-7x7.json')
+            static::getSharedFixtureContent('game-initiation-requests/valid/valid-1-opponent-7x7.json')
         );
         $this->assertSuccessfulJSONResponse($client->getResponse());
 
@@ -93,7 +93,7 @@ class GameControllerTest extends AbstractControllerTestCase
             [],
             [],
             ['CONTENT_TYPE' => 'application/json', 'HTTP_accept' => 'application/xml'],
-            static::getSharedFixtureContent('game-initiation-requests/valid/2-players-7x7.json')
+            static::getSharedFixtureContent('game-initiation-requests/valid/valid-1-opponent-7x7.json')
         );
 
         $this->assertSuccessfulXMLResponse($client->getResponse());
