ae20e29a3debbca31b3d5482d94fb8b14cdce84c
Eugene Matvejev <eugene.matvejev@gmail.com>
2018-01-09 11:48:40 +0000

    Update MockFactory.php

--- tests/shared-environment/Factory/MockFactory.php
+++ tests/shared-environment/Factory/MockFactory.php
@@ -12,6 +12,14 @@ use EM\FoundationBundle\Model\UserModel;
  */
 class MockFactory
 {
+    public static function getBattlefieldMock(int $size = 7) : Battlefield
+    {
+        $battlefield = BattlefieldModel::generate($size)
+            ->setUser(static::getUserMock(''));
+
+        return $battlefield;
+    }
+
     public static function getCellMock(string $coordinate, int $mask = CellModel::FLAG_NONE) : Cell
     {
         return (new Cell())
@@ -30,14 +38,6 @@ class MockFactory
             ->setEmail($email)
             ->setPasswordHash(sha1('mockedPassword'))
             ->setFlags($flags);
-    }
-
-    public static function getBattlefieldMock(int $size = 7) : Battlefield
-    {
-        $battlefield = BattlefieldModel::generate($size)
-            ->setUser(static::getUserMock(''));
-
-        return $battlefield;
     }
 
     public static function getGameMock(int $players = 2, int $size = 7) : Game
