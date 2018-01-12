c5d4ee7343343d911151b3986bb71f5edace4b85
Eugene Matvejev <eugene.matvejev@gmail.com>
2018-01-12 15:00:23 +0000

    Create PlayerException.php

---
+++ FoundationBundle/Exception/PlayerException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EM\FoundationBundle\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * @since 9.2
+ */
+class PlayerException extends HttpException
+{
+}
